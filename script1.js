d7cb9eb412d715977397befe4e7f8164ad6928f3
Ziyanda Dyani <ziyandadyani17@gmail.com>
2023-04-05 10:47:16 +0200

    Debugging and fixing code syntax with IWA6 challenge 1 code

--- script1.js
+++ script1.js
@@ -9,17 +9,24 @@
 // console.log('Primary phone is valid numerical string:', primaryValid)
 // console.log('Secondary phone is valid numerical string:', secondaryValid )
 
+
+
+
 const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
-
-
-const primaryValid =  Number(primaryPhone) !== NaN
-const secondaryValid = Number(secondaryPhone) !== NaN
-
-//
 //console.log(Number(primaryPhone) , Number(secondaryPhone) )
-//
+
+//const primaryValid =  Number(primaryPhone) !== NaN  // Doesnt work because NaN is not equal to itself NaN !== NaN will always be true 
+//const secondaryValid = Number(secondaryPhone) !== NaN
+
+const primaryValid = Object.is(NaN,Number(primaryPhone)) ? false : true
+const secondaryValid = Object.is(NaN,Number(secondaryPhone)) ? false : true
+
+// The code above checks whether the string telephone numbers, when converted to the number type , are equal to a NaN value using 
+//Object.is , if condition is true true then it means its not a number (not NaN) and we assign the value of false to 
+// const primaryValid/secondaryValid (validation), if false this means the string does have numerical digits and a value of true is
+// assigned to const primaryValid/secondaryValid (validation)
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
